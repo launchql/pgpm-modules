1807fa336e053d6841bdca8b40151e6a17ca2b6b
Dan Lynch <pyramation@gmail.com>
2020-12-08 19:03:09 -0700

    updates

--- packages/db_meta/sql/db_meta--0.1.5.sql
+++ packages/db_meta/sql/db_meta--0.1.5.sql
@@ -291,11 +291,13 @@ CREATE INDEX domains_database_id_idx ON meta_public.domains ( database_id );
 CREATE TABLE meta_public.apis (
  	id uuid PRIMARY KEY DEFAULT ( uuid_generate_v4() ),
 	database_id uuid NOT NULL REFERENCES collections_public.database ( id ) ON DELETE CASCADE,
+	name text NOT NULL,
 	domain_id uuid NOT NULL,
 	schemas text[] NOT NULL,
 	dbname text NOT NULL DEFAULT ( current_database() ),
 	role_name text NOT NULL DEFAULT ( 'authenticated' ),
 	anon_role text NOT NULL DEFAULT ( 'anonymous' ),
+	UNIQUE ( database_id, name ),
 	UNIQUE ( domain_id ) 
 );
 
