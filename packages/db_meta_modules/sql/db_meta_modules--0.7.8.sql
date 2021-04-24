8689b830ce948b01f7285679db67996ebc111b87
Dan Lynch <pyramation@gmail.com>
2021-04-24 04:42:48 -0700

    pkg

--- packages/db_meta_modules/sql/db_meta_modules--0.7.8.sql
+++ packages/db_meta_modules/sql/db_meta_modules--0.7.8.sql
@@ -104,13 +104,15 @@ CREATE TABLE meta_public.denormalized_table_field (
 	field_id uuid NOT NULL,
 	set_ids uuid[],
 	ref_table_id uuid NOT NULL,
+	ref_field_id uuid NOT NULL,
 	ref_ids uuid[],
 	use_updates bool NOT NULL DEFAULT ( TRUE ),
 	update_defaults bool NOT NULL DEFAULT ( TRUE ),
 	CONSTRAINT db_fkey FOREIGN KEY ( database_id ) REFERENCES collections_public.database ( id ) ON DELETE CASCADE,
 	CONSTRAINT table_fkey FOREIGN KEY ( table_id ) REFERENCES collections_public."table" ( id ) ON DELETE CASCADE,
 	CONSTRAINT ref_table_fkey FOREIGN KEY ( ref_table_id ) REFERENCES collections_public."table" ( id ) ON DELETE CASCADE,
-	CONSTRAINT field_fkey FOREIGN KEY ( field_id ) REFERENCES collections_public.field ( id ) ON DELETE CASCADE 
+	CONSTRAINT field_fkey FOREIGN KEY ( field_id ) REFERENCES collections_public.field ( id ) ON DELETE CASCADE,
+	CONSTRAINT ref_field_fkey FOREIGN KEY ( ref_field_id ) REFERENCES collections_public.field ( id ) ON DELETE CASCADE 
 );
 
 COMMENT ON CONSTRAINT db_fkey ON meta_public.denormalized_table_field IS E'@omit manyToMany';
@@ -120,6 +122,8 @@ COMMENT ON CONSTRAINT table_fkey ON meta_public.denormalized_table_field IS E'@o
 COMMENT ON CONSTRAINT ref_table_fkey ON meta_public.denormalized_table_field IS E'@omit manyToMany';
 
 COMMENT ON CONSTRAINT field_fkey ON meta_public.denormalized_table_field IS E'@omit manyToMany';
+
+COMMENT ON CONSTRAINT ref_field_fkey ON meta_public.denormalized_table_field IS E'@omit manyToMany';
 
 CREATE INDEX denormalized_table_field_database_id_idx ON meta_public.denormalized_table_field ( database_id );
 
